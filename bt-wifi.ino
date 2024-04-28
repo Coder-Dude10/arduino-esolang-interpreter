2917807553357a112d6521f5cea1e6e526066567
Alexander Gaggl <106555946+Coder-Dude10@users.noreply.github.com>
2024-04-27 20:44:08 -0500

    Update bt-wifi.ino

--- bt-wifi.ino
+++ bt-wifi.ino
@@ -7,11 +7,12 @@ int currentCoordinate = 0;
 int currentCharValue = 0;
 int inputClock = 0;
 int shift = 0;
-char inChar = 'a';
-char dataChar = 'a';
-char ascii[55] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '+', '-', '<', '>', '[', ']', ',', '.', '?', '!', ':', '~', '^', '=', '@', '*', '/', '\"'};
+int commands[18] = {43, 45, 60, 62, 91, 93, 44, 46, 63, 33, 58, 126, 94, 61, 64, 42, 47, 34};
 bool readReady = false;
 bool enter = false;
+char inChar = 'a';
+char dataChar = 'a';
+String charNames[7] = {"[heart]", "[checkMark]", "[crossMark]", "[apple]", "[banana]", "[entity]", "[deleteChar]"};
 
 void setup() {
     Serial.begin(9600);
@@ -19,10 +20,41 @@ void setup() {
     pinMode(2, INPUT);
     pinMode(3, INPUT);
     pinMode(4, INPUT);
-    pinMode(5, INPUT);
+    pinMode(5, OUTPUT);
     pinMode(6, OUTPUT);
     pinMode(7, OUTPUT);
-    pinMode(8, OUTPUT);
+}
+
+void updateShiftLayer() {
+    if (shift == 3) {
+        digitalWrite(5, HIGH);
+        digitalWrite(6, HIGH);
+        digitalWrite(7, LOW);
+    } else {
+        for (int i = 0; i < 3; i++) {
+            if (shift == i) {
+                digitalWrite(i + 5, HIGH);
+            } else {
+                digitalWrite(i + 5, LOW);
+            }
+        }
+    }
+}
+
+int ascii(int asciiValue) {
+    if (asciiValue < 10) {
+        return (asciiValue + 48);
+    } else {
+        if (asciiValue < 37) {
+            if (asciiValue == 36) {
+                return 32;
+            } else {
+                return (asciiValue + 55);
+            }
+        } else {
+            return commands[asciiValue - 37];
+        }
+    }
 }
 
 void loop() {
@@ -36,16 +68,7 @@ void loop() {
                 if (enter) {
                     Serial.print('e');
                 } else {
-                    if (shift > 0 && currentCharValue < 18) {
-                        Serial.print(ascii[currentCharValue + 37]);
-                    } else {
-                        Serial.print(ascii[currentCharValue % 37]);
-                    }
-                }
-                
-                if (shift == 1) {
-                    shift = 0;
-                    updateShiftIndicator();
+                    Serial.print(char(ascii(currentCharValue + (shift * 14))));
                 }
                 
                 enter = false;
@@ -53,11 +76,15 @@ void loop() {
             
             if (inChar == '1') {
                 shift = int(dataChar) - 48;
-                updateShiftIndicator();
+                updateShiftLayer();
             }
             
             if (inChar == '2') {
-                bleKeyboard.print(dataChar);
+                if (int(dataChar) < 128) {
+                    bleKeyboard.print(dataChar);
+                } else {
+                    bleKeyboard.print(charNames[int(dataChar) - 128]);
+                }
             }
             
             dataChar = 'a';
@@ -68,14 +95,14 @@ void loop() {
     
     if (currentCoordinate != pastCoordinate && readReady) {
         if (currentCoordinate == (pastCoordinate + 1) || (pastCoordinate - currentCoordinate) == 2) {
-            if (currentCharValue == 37) {
+            if (currentCharValue == 14) {
                 currentCharValue = 0;
             } else {
                 currentCharValue++;
             }
         } else {
             if (currentCharValue == 0) {
-                currentCharValue = 37;
+                currentCharValue = 14;
             } else {
                 currentCharValue--;
             }
@@ -83,46 +110,39 @@ void loop() {
 
         pastCoordinate = currentCoordinate;
     }
-
+    
     if (digitalRead(4) == 1) {
-        readReady = true;
-        digitalWrite(6, HIGH);
-    }
-
-    if (digitalRead(5) == 1 && readReady) {
         inputClock = 0;
         
-        while (digitalRead(5) == 1 && inputClock < 100) {
+        while (digitalRead(4) == 1 && inputClock < 100) {
             delay(10);
             inputClock++;
         }
 
         if (inputClock == 100) {
+            if (readReady) {
+                digitalWrite(6, LOW);
+            } else {
+                digitalWrite(6, HIGH);
+            }
+            
+            shift = 0;
+            enter = false;
             currentCharValue = 0;
-            readReady = false;
-            digitalWrite(6, LOW);
+            currentCoordinate = 0;
+            readReady = !(readReady);
         } else {
-            if (currentCharValue == 37) {
-                if (shift == 2) {
+            if (currentCharValue == 14) {
+                if (shift == 3) {
                     shift = 0;
                 } else {
                     shift++;
                 }
 
-                updateShiftIndicator();
+                updateShiftLayer();
             } else {
                 enter = true;
             }
-        }
-    }
-}
-
-void updateShiftIndicator() {
-    for (int i = 0; i < 2; i++) {
-        if (shift == i) {
-            digitalWrite(i + 6, HIGH);
-        } else {
-            digitalWrite(i + 6, LOW);
         }
     }
 }
